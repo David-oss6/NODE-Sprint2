c73799a3e191dda0417e0665a2bc415f1c0a5331
David Iglesias <im.david85@gmail.com>
2023-02-14 20:26:13 +0100

    sprint finalitzat

---
+++ entrega23/Nivell 1/diagrama-optica/proveidro_data.js
@@ -0,0 +1,116 @@
+[
+  {
+    nom: "proveidor1",
+    adreca: {
+      carrer: "C/Barcelona",
+      num: 5,
+      pis: 2,
+      porta: 1,
+      ciudad: "Barcelona",
+      "codi postal": 8022,
+      pais: "ESP",
+    },
+    tel: 654987321,
+    fax: null,
+    nif: "456123l",
+    marques: [
+      {
+        marca1: {
+          id: 1,
+          nom: "Marca1",
+          ulleres: []
+        },
+        marca2: {
+          id: 1,
+          nom: "Marca2",
+          ulleres: [
+            {
+              id: 1,
+              "grad esquerra": 1,
+              "grad dreta": 0.5,
+              montura: "pasta",
+              "color montura": "verd",
+              "color vidre": "blanc",
+              preu: 9,
+            },
+            {
+              id: 2,
+              "grad esquerra": 1,
+              "grad dreta": 0.5,
+              montura: "metalica",
+              "color montura": "verd",
+              "color vidre": "blanc",
+              preu: 8,
+            },
+            {
+              id: 3,
+              "grad esquerra": 1,
+              "grad dreta": 0.5,
+              montura: "flotant",
+              "color montura": "verd",
+              "color vidre": "blanc",
+              preu: 10,
+            },
+          ],
+        },
+      },
+    ],
+  },
+  {
+    nom: "proveidor2",
+    adreca: {
+      carrer: "C/Valencia",
+      num: 2,
+      pis: 2,
+      porta: 1,
+      ciudad: "Valencia",
+      "codi postal": 8033,
+      pais: "ESP",
+    },
+    tel: 987456123,
+    fax: null,
+    nif: "98545m",
+    marques: [
+      {
+        marca1: {
+          id: 1,
+          nom: "Marca1",
+          ulleres: [],
+        },
+        marca2: {
+          id: 1,
+          nom: "Marca2",
+          ulleres: [
+            {
+              id: 1,
+              "grad esquerra": 1,
+              "grad dreta": 0.5,
+              montura: "pasta",
+              "color montura": "verd",
+              "color vidre": "blanc",
+              preu: 9,
+            },
+            {
+              id: 2,
+              "grad esquerra": 1,
+              "grad dreta": 0.5,
+              montura: "metalica",
+              "color montura": "verd",
+              "color vidre": "blanc",
+              preu: 8,
+            },
+            {
+              id: 3,
+              "grad esquerra": 1,
+              "grad dreta": 0.5,
+              montura: "flotant",
+              "color montura": "verd",
+              "color vidre": "blanc",
+              preu: 10,
+            },
+          ],
+        },
+      },
+    ],
+  },
+];
